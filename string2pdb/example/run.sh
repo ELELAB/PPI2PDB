74abb74530ae73f50d35bdc301067cfe65b05804
Eleni Kiahaki <s222777@dtu.dk>
2025-10-13 17:16:44 +0200

    addition of afmulti input files generation in string2pdb

--- string2pdb/example/run.sh
+++ string2pdb/example/run.sh
@@ -1,2 +1,2 @@
 module load python/3.10/modulefile
-./../string2pdb O15315
+./../string2pdb O15315 -a
--- string2pdb/example2/run.sh
+++ string2pdb/example2/run.sh
@@ -1,2 +1,2 @@
 module load python/3.10/modulefile
-./../string2pdb Q99728 -t 0.9 -n functional
+./../string2pdb Q99728 -t 0.9 -n functional -a
